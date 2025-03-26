304089f92fa5ebc619b8d1c3130980ae393cbee4
KennObby <oleg.ilyine@gmail.com>
2025-03-26 14:03:38 +0100

    minishell starter kit

---
+++ inc/minishell.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oilyine- <oleg.ilyine@student42.luxembour  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/25 10:01:37 by oilyine-          #+#    #+#             */
+/*   Updated: 2025/03/25 10:04:49 by oilyine-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+//< ----------------------- INCLUDES -------------------- >
+# include <stdio.h>
+# include <stdint.h>
+# include <stdarg.h>
+# include <stdlib.h>
+# include <signal.h>
+
+# endif
